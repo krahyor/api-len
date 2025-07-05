05ad971de69624bce3a321b4a243c8791d27ea45
krahyor <gimgim611@gmail.com>
2025-07-05 21:36:11 +0700

    ✨ feat:add customer

---
+++ apilen/routers/v1/__init__.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from . import (
+    customer_router,
+    station_router,
+    vehicle_router,
+    delivery_staff_router,
+    parcel_router,
+)
+
+router = APIRouter(prefix="/v1")
+router.include_router(customer_router.router)
+router.include_router(station_router.router)
+router.include_router(vehicle_router.router)
+router.include_router(delivery_staff_router.router)
+router.include_router(parcel_router.router)
